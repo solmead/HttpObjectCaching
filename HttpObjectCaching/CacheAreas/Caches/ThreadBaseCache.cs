c400904cd7597b01c2d540d905269556b16b07db
Chris Powell <powelcr@ucmail.uc.edu>
2014-09-29 20:28:36 -0400

    Fix thread issue

--- HttpObjectCaching/CacheAreas/Caches/ThreadBaseCache.cs
+++ HttpObjectCaching/CacheAreas/Caches/ThreadBaseCache.cs
@@ -1,44 +1,63 @@
-﻿//using System;
-//using System.Threading;
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using HttpObjectCaching.Helpers;
 
-//namespace HttpObjectCaching.CacheAreas.Caches
-//{
-//    public class ThreadBaseCache : ICacheArea
-//    {
-//        public CacheArea Area { get { return CacheArea.Thread; } }
-//        public string Name { get { return "BaseThread"; } }
-//        public void ClearCache()
-//        {
-//            throw new NotImplementedException();
-//        }
+namespace HttpObjectCaching.CacheAreas.Caches
+{
+    public class ThreadBaseCache : ICacheArea
+    {
+        public CacheArea Area { get { return CacheArea.Thread; } }
+        public string Name { get { return "BaseThread"; } }
+        public void ClearCache()
+        {
+            throw new NotImplementedException();
+        }
 
-//        public tt GetItem<tt>(string name, Func<tt> createMethod = null, double? lifeSpanSeconds = null)
-//        {
-//            try
-//            {
-//                var t = (tt)Thread.GetData(Thread.GetNamedDataSlot(name.ToUpper()));
-//                object comp = t;
-//                object empty = default(tt);
-//                if (comp == empty)
-//                {
-//                    if (createMethod != null)
-//                    {
-//                        t = createMethod();
-//                        SetItem(name, t, lifeSpanSeconds);
-//                    }
-//                }
-//                return t;
-//            }
-//            catch
-//            {
-//                throw;
-//            }
-//            return default(tt);
-//        }
+        public tt GetItem<tt>(string name, Func<tt> createMethod = null, double? lifeSpanSeconds = null)
+        {
+            object empty = default(tt);
+            tt tObj = default(tt);
+            var entry = Thread.GetData(Thread.GetNamedDataSlot(name.ToUpper())) as CachedEntry;
+            if (entry == null || (entry.TimeOut.HasValue && entry.TimeOut.Value < DateTime.Now))
+            {
+                entry = new CachedEntry();
+            }
+            try
+            {
+                tObj = (tt)(entry.Item);
+            }
+            catch (Exception)
+            {
 
-//        public void SetItem<tt>(string name, tt obj, double? lifeSpanSeconds = null)
-//        {
-//            Thread.SetData(Thread.GetNamedDataSlot(name.ToUpper()), obj);
-//        }
-//    }
-//}
+            }
+            object comp = tObj;
+            if (comp == empty)
+            {
+                if (createMethod != null)
+                {
+                    tObj = createMethod();
+                }
+            }
+            SetItem(name, tObj, lifeSpanSeconds);
+            return tObj;
+        }
+
+        public void SetItem<tt>(string name, tt obj, double? lifeSpanSeconds = null)
+        {
+            var entry = new CachedEntry()
+            {
+                Name = name,
+                Changed = DateTime.Now,
+                Created = DateTime.Now,
+                Item = obj
+            };
+            if (lifeSpanSeconds.HasValue)
+            {
+                entry.TimeOut = DateTime.Now.AddSeconds(lifeSpanSeconds.Value);
+            }
+
+            Thread.SetData(Thread.GetNamedDataSlot(name.ToUpper()), entry);
+        }
+    }
+}
--- HttpObjectCaching/CacheAreas/Caches/ThreadCache.cs
+++ HttpObjectCaching/CacheAreas/Caches/ThreadCache.cs
@@ -1,17 +1,17 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-//namespace HttpObjectCaching.CacheAreas.Caches
-//{
-//    public class ThreadCache : DictionaryCache
-//    {
-//        public ThreadCache()
-//            : base(new ThreadBaseCache(), () => "thread")
-//        {
-//            Area = CacheArea.Thread;
-//            Name = "DefaultThread";
-//        }
-//    }
-//}
+namespace HttpObjectCaching.CacheAreas.Caches
+{
+    public class ThreadCache : DictionaryCache
+    {
+        public ThreadCache()
+            : base(new ThreadBaseCache(), () => "thread", 1)
+        {
+            Area = CacheArea.Thread;
+            Name = "DefaultThread";
+        }
+    }
+}
