4d27a0a4a05008fc1a99c6c6862ea657b164c4d1
Christopher Powell <powelcr@ucmail.uc.edu>
2015-05-15 14:31:41 -0400

    Added initial Set Collection

---
+++ HttpObjectCaching/Core/Collections/SetOnCache.cs
@@ -0,0 +1,105 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace HttpObjectCaching.Core.Collections
+//{
+//    public class SetOnCache<tt> : ISet<tt>
+//    {
+//        public IEnumerator<tt> GetEnumerator()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        IEnumerator IEnumerable.GetEnumerator()
+//        {
+//            return GetEnumerator();
+//        }
+
+//        public void Add(tt item)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void UnionWith(IEnumerable<tt> other)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void IntersectWith(IEnumerable<tt> other)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void ExceptWith(IEnumerable<tt> other)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void SymmetricExceptWith(IEnumerable<tt> other)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool IsSubsetOf(IEnumerable<tt> other)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool IsSupersetOf(IEnumerable<tt> other)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool IsProperSupersetOf(IEnumerable<tt> other)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool IsProperSubsetOf(IEnumerable<tt> other)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool Overlaps(IEnumerable<tt> other)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool SetEquals(IEnumerable<tt> other)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool Add(tt item)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void Clear()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool Contains(tt item)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void CopyTo(tt[] array, int arrayIndex)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool Remove(tt item)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public int Count { get; private set; }
+//        public bool IsReadOnly { get; private set; }
+//    }
+//}
